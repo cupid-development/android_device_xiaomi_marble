42500f971d137d7c306c3a54b29416793321bc67
Jens Reidel <adrian@travitia.xyz>
2023-05-11 17:53:50 +0200

    marble: Initial device tree
    Change-Id: I6d195d295b5f94b71c3ff32b21aa33d3af7bf588
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ BoardConfig.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm8450-common
+include device/xiaomi/sm8450-common/BoardConfigCommon.mk
+
+# Inherit from the proprietary version
+include vendor/xiaomi/marble/BoardConfigVendor.mk
+
+DEVICE_PATH := device/xiaomi/marble
---
+++ lineage_marble.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from marble device
+$(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Inherit from common lineage configuration
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_marble
+PRODUCT_DEVICE := marble
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := 23049PCD8G
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
