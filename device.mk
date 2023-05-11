0e024e48ed0454aa56773b71ef4bd9f16448d970
Jens Reidel <adrian@travitia.xyz>
2023-05-11 17:53:50 +0200

    marble: Initial device tree
    Change-Id: I6d195d295b5f94b71c3ff32b21aa33d3af7bf588
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

---
+++ device.mk
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2022-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from xiaomi sm8450-common
+$(call inherit-product, device/xiaomi/sm8450-common/common.mk)
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
