f12906947aeeff8aff39dfce2427a6809f5f23fd
Arian <arian.kulmer@web.de>
2023-06-25 14:43:55 +0200

    marble: Import audio configs from V14.0.9.0.TMREUXM
    Change-Id: I32c54ebd2f55ed06bc49da567d6654f056909f50

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_overlay_static.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ukee/mixer_paths_overlay_static.xml \
+    $(LOCAL_PATH)/audio/resourcemanager_ukee_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_ukee/resourcemanager_ukee_mtp.xml
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMarble \
