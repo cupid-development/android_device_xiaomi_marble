80d32670c065435de2472b0804144bfd6fb9c81e
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:57:49 -0500

    marble: Add vendor soong imports to extract
    Change-Id: I7b290a9412bd11bb5577ce634a0291ffca40b283

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -6,6 +6,21 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function vendor_imports() {
+    cat << EOF >> "$1"
+		"device/xiaomi/sm8450-common",
+		"hardware/qcom-caf/sm8450",
+		"hardware/xiaomi",
+		"vendor/qcom/opensource/commonsys-intf/display",
+EOF
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
 set -e
 
 export DEVICE=marble
