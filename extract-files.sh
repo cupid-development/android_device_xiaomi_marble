8b1966fed8bded989e8fb9bc5d7ab836f314b985
Jens Reidel <adrian@travitia.xyz>
2023-11-15 19:28:53 +0100

    marble: Initial proprietary-files.txt
    Change-Id: Ie1ed5a32dd3c97b035096f0b068e53e095900a2e
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,15 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/camera/marble_enhance_motiontuning.xml|vendor/etc/camera/marble_motiontuning.xml)
+            sed -i 's/xml=version/xml version/g' "${2}"
+            ;;
+        vendor/etc/camera/pureView_parameter.xml)
+            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
+            ;;
+        vendor/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
+            sed -i 's/_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE/_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0/g' "${2}"
+            ;;
     esac
 }
 
