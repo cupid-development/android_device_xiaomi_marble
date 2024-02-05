e7059d5d396d90a045180a389f09e2bba1d6f9f0
Jens Reidel <adrian@travitia.xyz>
2024-02-05 09:35:33 +0100

    marble: Set name to Redmi Note 12 Turbo on Chinese phones
    Change-Id: I79348a63f8dbc303d01331de73550b76e8b353f6
    Signed-off-by: Jens Reidel <adrian@travitia.xyz>

--- device.mk
+++ device.mk
@@ -10,14 +10,20 @@ $(call inherit-product, device/xiaomi/sm8450-common/common.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/marble/marble-vendor.mk)
 
+# Init scripts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.marble.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.marble.rc
+
 # Overlay
 PRODUCT_PACKAGES += \
     ApertureResMarble \
     FrameworksResMarble \
     SettingsProviderResMarble \
+    SettingsProviderResMarbleCN \
     SettingsResMarble \
     SystemUIResMarble \
-    WifiResMarble
+    WifiResMarble \
+    WifiResMarbleCN \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
