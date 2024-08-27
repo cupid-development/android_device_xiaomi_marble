a9c64d1668e349dea544d332bc8b1f46de13617d
Michael Bestas <mkbestas@lineageos.org>
2024-08-27 22:04:35 +0300

    marble: Sync extract scripts with templates
    Change-Id: I98a36854c5a0c0390592911bb2558b34eff77efc

--- extract-files.sh
+++ extract-files.sh
@@ -1,35 +1,48 @@
 #!/bin/bash
 #
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
-#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 function blob_fixup() {
     case "${1}" in
         vendor/etc/camera/marble_enhance_motiontuning.xml|vendor/etc/camera/marble_motiontuning.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/xml=version/xml version/g' "${2}"
             ;;
         vendor/etc/camera/pureView_parameter.xml)
+            [ "$2" = "" ] && return 0
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
         vendor/lib64/hw/audio.primary.taro-marble.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "audio.primary.taro-marble.so" "${2}"
             ;;
         vendor/lib64/hw/fingerprint.fpc.default.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "fingerprint.fpc.default.so" "${2}"
             ;;
         vendor/lib64/hw/fingerprint.goodix.default.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --set-soname "fingerprint.goodix.default.so" "${2}"
             ;;
         vendor/lib64/libcamximageformatutils.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "vendor.qti.hardware.display.config-V2-ndk_platform.so" "vendor.qti.hardware.display.config-V2-ndk.so" "${2}"
             ;;
         vendor/lib64/libkaraokepal.so)
+            [ "$2" = "" ] && return 0
             "${PATCHELF_0_17_2}" --replace-needed "audio.primary.taro.so" "audio.primary.taro-marble.so" "${2}"
             ;;
+        *)
+            return 1
+            ;;
     esac
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # If we're being sourced by the common script that we called,
@@ -43,5 +56,6 @@ set -e
 export DEVICE=marble
 export DEVICE_COMMON=sm8450-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
